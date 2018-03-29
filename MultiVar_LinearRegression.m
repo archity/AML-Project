1e58a851993b248b7bdbc109f4d6dbbcf2651720
Archit Yadav <archityadav97@gmail.com>
2018-03-29 19:29:17 +0530

    Improved code
    The cost function now does go to almost zero (0.04) from 10^3. Curve fitting still not working correctly.

--- MultiVar_LinearRegression.m
+++ MultiVar_LinearRegression.m
@@ -4,44 +4,97 @@ close all
 
 fileToRead = 'S&Pdata'; 
 
+rangeTaken = 1:100;
+
 % Import the complete spreadsheet file
 [xlsObjectComplete, xlsHeads] = xlsread(fileToRead);
-% xlsHeads contains the headings in the form of a string vector
+% xlsHeads contains the headings in the form of a string
 
-% Filter just the S&P Close into a vector
-SP_Close = xlsObjectComplete(:, 22);
+xlsHeads = xlsHeads(2:22);  % Remove the 'DATE' heading
 
-inputMatrix = xlsObjectComplete(:, 2:21);
+% Filter just the S&P Close into a vector (Take the last col only)
+SP_Close = xlsObjectComplete(rangeTaken, 22);
+
+inputMatrix = xlsObjectComplete(rangeTaken, 2:21);  
 
 thetaWeights = zeros(21, 1);
 
 % Learning rate, or rate of descent
-alpha = 0.00000000000001;
-iterations = 10000;
+alpha = 0.01;
+iterations = 1000;
+
+%--------------------------------------------------------------------------
+% Normalization
+%--------------------------------------------------------------------------
+
+X = inputMatrix;
+X_norm = X;
+mu = zeros(1, size(X, 2));
+sigma = zeros(1, size(X, 2));
+
+for feature_index = 1:size(X,2)
+ 
+    % Find mean
+    feature_mean = mean(X(:,feature_index));
+    % (datatpoint - mean)
+    X_norm(:,feature_index) = X(:,feature_index) - feature_mean;
+ 
+    % Find StdDev
+    feature_std = std(X_norm(:,feature_index));
+    % (datatpoint - mean)/(stdDev)
+    X_norm(:,feature_index) = X_norm(:,feature_index) / feature_std;    
+ 
+    sigma(feature_index) = feature_std;
+    mu(feature_index) = feature_mean;
+end
+
+
 
 %--------------------------------------------------------------------------
 % Gradient descent algo
 %--------------------------------------------------------------------------
 
 m = length(SP_Close);
+
+X_norm = [ones(m, 1) X_norm];   % The normalized X
 X = [ones(m, 1) inputMatrix];
+
 y = SP_Close;
 
 for i = 1 : iterations
-    temp = thetaWeights - (alpha/m) * X' * (X * thetaWeights - y);
+    temp = thetaWeights - (alpha/m) * X_norm' * (X_norm * thetaWeights - y);
     thetaWeights = temp;
     
-    
-    jHistory(i) = (1 / (2*m) ) * sum(((X * thetaWeights)-y).^2);
+    jHistory(i) = (1 / (2*m) ) * sum(((X_norm * thetaWeights)-y).^2);
 end
 
 %--------------------------------------------------------------------------
 % Cost function algo
 %--------------------------------------------------------------------------
 J = 0;
-J = (1 / (2*m) ) * sum(((X * thetaWeights)-y).^2);
+J = (1 / (2*m) ) * sum(((X_norm * thetaWeights)-y).^2);
 
-plot(1:iterations, jHistory);
-xlabel('Iterations'); ylabel('J');
+% plot(1:iterations, jHistory);
+% xlabel('Iterations'); ylabel('J');
 
 jHistory = jHistory';
+
+
+%--------------------------------------------------------------------------
+% Plot the results
+%--------------------------------------------------------------------------
+
+figure;
+i = 2;  % 2nd column (feature) of input matrix
+
+% scatter(x, y)
+scatter(inputMatrix(:, i), SP_Close);
+xlabel( xlsHeads(i) ); ylabel('S&P Close');
+
+hTheta = inputMatrix(:, i) * thetaWeights(i);
+
+% Plot the linear fit
+hold on; % keep previous plot visible
+plot( inputMatrix(:, i), inputMatrix(:, i) * thetaWeights(i), '-' )
+legend('Training data', 'Linear regression')
+hold off % don't overlay any more plots on this figure
