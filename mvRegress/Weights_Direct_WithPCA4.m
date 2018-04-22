7ae6c8d22fa4b9ba8c409f1b82462a3e0bb7b5f8
Archit Yadav <archityadav97@gmail.com>
2018-04-23 00:21:54 +0530

    PCA 4 & 5

---
+++ mvRegress/Weights_Direct_WithPCA4.m
@@ -0,0 +1,39 @@
+clc;
+clear variables;
+close all;
+
+fileToRead = 'PCA_4.xlsx';
+
+
+% Import the complete spreadsheet file
+[xlsObjectComplete] = xlsread(fileToRead);
+% xlsHeads contains the headings in the form of a string vector
+
+%%
+% Filter just the S&P Close into a vector
+SP_Close_ToBePredicted = xlsObjectComplete(:, 17);
+
+input = xlsObjectComplete(:, 1:16);
+output = xlsObjectComplete(:, 17);
+
+coeff = mvregress(input, output);
+% Returns the estimated coefficients for a multivariate normal regression 
+
+prediction = input(1:679, 1:16).* coeff';
+for i = 1 : 679
+    yCap(i) = sum(prediction(i, :)); 
+end
+yCap = yCap';
+
+numerator = sum( (SP_Close_ToBePredicted - yCap).^2 );
+denominator = sum( ( SP_Close_ToBePredicted - mean(SP_Close_ToBePredicted) ).^2 );
+accu = 1 - (numerator/denominator)
+
+
+mse = (1/679) * numerator
+
+plot(1:679, SP_Close_ToBePredicted);
+hold on
+plot(1:679, yCap);
+legend('Actual', 'Predicted');
+xlabel('dataPoints'); ylabel('y');
