aac13356cc5e18e226a9e513cf8ad86edffa219a
Archit Yadav <archityadav97@gmail.com>
2018-04-22 22:03:39 +0530

    mvregress on all dataPoints
    An almost 100% accuracy !?

--- mvRegress/Weights_Direct.m
+++ mvRegress/Weights_Direct.m
@@ -12,18 +12,33 @@ rangeTaken = 1:550;
 
 xlsComplete = xlsObjectComplete;
 
+% Filter just the S&P Close into a vector
+SP_Close_ToBePredicted = xlsObjectComplete(:, 22);
+
 xlsHeads = xlsHeads(2:21);  % Remove the 'DATE' heading
 xlsObjectComplete = xlsObjectComplete(:, 2:22); % Remove the date
 input = xlsObjectComplete(:, 1:20);
 output = xlsObjectComplete(:, 21);
 
 coeff = mvregress(input, output);
-prediction = input(241, 1:20).*coeff';
-%% 
-prediction = sum(prediction);
+% Returns the estimated coefficients for a multivariate normal regression 
 
-svm_model = fitrsvm(input, output);
-%% 
+prediction = input(1:679, 1:20).* coeff';
+for i = 1 : 679
+    yCap(i) = sum(prediction(i, :)); 
+end
+yCap = yCap';
 
-prediction_svm = predict(svm_model, input(300, 1:20));
-prediction_svm
+numerator = sum( (SP_Close_ToBePredicted - yCap).^2 );
+denominator = sum( ( SP_Close_ToBePredicted - mean(SP_Close_ToBePredicted) ).^2 );
+accu = 1 - (numerator/denominator)
+
+
+mse = (1/679) * numerator
+
+plot(1:679, SP_Close_ToBePredicted);
+hold on
+plot(1:679, yCap);
+legend('Actual', 'Predicted');
+xlabel('dataPoints'); ylabel('y');
+
