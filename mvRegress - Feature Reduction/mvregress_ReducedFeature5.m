394ee1edd11ee2be48e78e032ffa4e469b7702df
Archit Yadav <archityadav97@gmail.com>
2018-04-23 01:11:51 +0530

    mvregress - Feature Reduction
    Feature reduction was applied by removing features based on the threashold value decided by correlation coefficient.

---
+++ mvRegress - Feature Reduction/mvregress_ReducedFeature5.m
@@ -0,0 +1,38 @@
+clc;
+clear variables;
+close all;
+
+fileToRead = 'S&Pdata_ReducedFeatures5.xlsx';
+
+% Import the complete spreadsheet file
+[xlsObjectComplete] = xlsread(fileToRead);
+% xlsHeads contains the headings in the form of a string vector
+
+%%
+% Filter just the S&P Close into a vector
+SP_Close_ToBePredicted = xlsObjectComplete(:, 8);
+
+input = xlsObjectComplete(:, 1:7);
+output = xlsObjectComplete(:, 8);
+
+coeff = mvregress(input, output);
+% Returns the estimated coefficients for a multivariate normal regression 
+
+prediction = input(1:679, 1:7).* coeff';
+for i = 1 : 679
+    yCap(i) = sum(prediction(i, :)); 
+end
+yCap = yCap';
+
+numerator = sum( (SP_Close_ToBePredicted - yCap).^2 );
+denominator = sum( ( SP_Close_ToBePredicted - mean(SP_Close_ToBePredicted) ).^2 );
+accu = 1 - (numerator/denominator)
+
+
+mse = (1/679) * numerator
+
+plot(1:679, SP_Close_ToBePredicted);
+hold on
+plot(1:679, yCap);
+legend('Actual', 'Predicted');
+xlabel('dataPoints'); ylabel('y');
